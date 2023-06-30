193f440bd2d3f2b59de7a5ab6f1c1accfbb43578
PoorGuy <poorguy.lg@gmail.com>
2023-07-01 01:12:22 +0800

    feat: new project

---
+++ testgit/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  testgit
+//
+//  Created by EHome.Liang on 2023/7/1.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ testgit/testgitApp.swift
@@ -0,0 +1,17 @@
+//
+//  testgitApp.swift
+//  testgit
+//
+//  Created by EHome.Liang on 2023/7/1.
+//
+
+import SwiftUI
+
+@main
+struct testgitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
